a13fa931f5fac0e101e325e2457c2aec3ec7a722
BARATHKUMAR M <101806993+BK0410@users.noreply.github.com>
2022-03-28 15:42:28 +0530

    Delete HungryProject directory

--- HungryProject/src/main/java/com/hungry/HungryProject/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hungry.HungryProject;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-
-	User save(User user);
-
-}
